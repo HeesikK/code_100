bcffda15457592d0d1b236841040593c1dbc04fa
HeesikK <7jjhkhz@naver.com>
2023-12-17 21:50:35 +0900

    feat: 9번 문제까지 풀이

---
+++ 1-100/q6.js
@@ -0,0 +1,13 @@
+/*
+다음중 True 값을 찾아라
+
+1) NaN (false)
+
+2) 1 (true)
+
+3) "" (false)
+
+4) 0 (false)
+
+5) undefined (false)
+*/
---
+++ 1-100/q9.js
@@ -0,0 +1,12 @@
+// concat 함수를 사용하여 2019/04/26 11:34:27 을 만들어라
+
+var year = "2019";
+var month = "04";
+var day = "26";
+var hour = "11";
+var minute = "34";
+var second = "27";
+
+var result = year.concat("/", month, "/", day, " ", hour, ":", minute, ":", minute);
+
+console.log(result);
